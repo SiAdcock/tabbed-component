3a8f62c65f1f1d074ebd758f8efbae3e62069c32
Simon Adcock <simonadcock2@gmail.com>
2016-03-06 14:35:51 +0000

    Fecth content using JSONP

--- app/modules/tabbed-component.js
+++ app/modules/tabbed-component.js
@@ -3,9 +3,47 @@ var APP = APP || {};
 APP.tabbedComponent = (function() {
   'use strict';
 
+  var sections = {
+    travel: {
+      url: 'http://content.guardianapis.com/search?format=json&section=travel&show-fields=trailText&page-size=5&api-key=9wur7sdh84azzazdt3ye54k4&callback=APP.tabbedComponent.render'
+    },
+    'uk-news': {
+      url: 'http://content.guardianapis.com/search?format=json&section=uk-news&show-fields=trailText&page-size=5&api-key=9wur7sdh84azzazdt3ye54k4&callback=APP.tabbedComponent.render'
+    },
+    football: {
+      url: 'http://content.guardianapis.com/search?format=json&section=football&show-fields=trailText&page-size=5&api-key=9wur7sdh84azzazdt3ye54k4&callback=APP.tabbedComponent.render'
+    }
+  };
+  var loadData = function(url) {
+    var script = document.createElement('script');
+
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+  var bindEvents = function() {
+    document.getElementById('travel').addEventListener('click', function() {
+      var url = sections.travel.url;
+
+      loadData(url);
+    });
+    document.getElementById('uk-news').addEventListener('click', function() {
+      var url = sections['uk-news'].url;
+
+      loadData(url);
+    });
+    document.getElementById('football').addEventListener('click', function() {
+      var url = sections.football.url;
+
+      loadData(url);
+    });
+  };
+
   return {
     init: function() {
-      console.log('Initialising');
+      bindEvents();
+    },
+    render: function(data) {
+      console.log('data', data.response.results);
     }
   };
 }());
