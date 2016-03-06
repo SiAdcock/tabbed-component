72740d4481225a2e74f4aa34e19edc939a2b0d95
Simon Adcock <simonadcock2@gmail.com>
2016-03-06 14:53:51 +0000

    Remove script node after JSONP request fired

--- app/modules/tabbed-component.js
+++ app/modules/tabbed-component.js
@@ -14,27 +14,28 @@ APP.tabbedComponent = (function() {
       url: 'http://content.guardianapis.com/search?format=json&section=football&show-fields=trailText&page-size=5&api-key=9wur7sdh84azzazdt3ye54k4&callback=APP.tabbedComponent.render'
     }
   };
-  var loadData = function(url) {
-    var script = document.createElement('script');
+  var loadData = function(url, sectionName) {
+    var scriptNode = document.createElement('script');
 
-    script.src = url;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    scriptNode.src = url;
+    document.getElementsByTagName('head')[0].appendChild(scriptNode);
+    scriptNode.parentNode.removeChild(scriptNode);
   };
   var bindEvents = function() {
     document.getElementById('travel').addEventListener('click', function() {
       var url = sections.travel.url;
 
-      loadData(url);
+      loadData(url, 'travel');
     });
     document.getElementById('uk-news').addEventListener('click', function() {
       var url = sections['uk-news'].url;
 
-      loadData(url);
+      loadData(url, 'uk-news');
     });
     document.getElementById('football').addEventListener('click', function() {
       var url = sections.football.url;
 
-      loadData(url);
+      loadData(url, 'football');
     });
   };
 
