e3812bed8529c4504ecad8354dfcaffb29ccaff0
Simon Adcock <simonadcock2@gmail.com>
2016-03-06 14:57:50 +0000

    Loading message

--- app/modules/tabbed-component.js
+++ app/modules/tabbed-component.js
@@ -14,34 +14,33 @@ APP.tabbedComponent = (function() {
       url: 'http://content.guardianapis.com/search?format=json&section=football&show-fields=trailText&page-size=5&api-key=9wur7sdh84azzazdt3ye54k4&callback=APP.tabbedComponent.render'
     }
   };
-  var loadData = function(url, sectionName) {
+  var showLoadingSpinner = function() {
+    document.getElementById('tab-content').innerHTML = 'Loading content';
+  };
+  var loadData = function(sectionName) {
     var scriptNode = document.createElement('script');
 
-    scriptNode.src = url;
+    showLoadingSpinner();
+    scriptNode.src = sections[sectionName].url;
     document.getElementsByTagName('head')[0].appendChild(scriptNode);
     scriptNode.parentNode.removeChild(scriptNode);
   };
   var bindEvents = function() {
     document.getElementById('travel').addEventListener('click', function() {
-      var url = sections.travel.url;
-
-      loadData(url, 'travel');
+      loadData('travel');
     });
     document.getElementById('uk-news').addEventListener('click', function() {
-      var url = sections['uk-news'].url;
-
-      loadData(url, 'uk-news');
+      loadData('uk-news');
     });
     document.getElementById('football').addEventListener('click', function() {
-      var url = sections.football.url;
-
-      loadData(url, 'football');
+      loadData('football');
     });
   };
 
   return {
     init: function() {
       bindEvents();
+      loadData('travel');
     },
     render: function(body) {
       var results = body.response.results;
